afd05d52e1d0bad410fa532487e66381e816fb64
Rodrigo <rodrigorochasantos@outlook.com>
2020-11-25 21:48:44 -0300

    Criaca do component nav para ser o menu de navegacao

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -8,18 +8,25 @@ import { HeaderComponent } from './components/template/header/header.component';
 
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { FooterComponent } from './components/template/footer/footer.component';
+import { NavComponent } from './components/template/nav/nav.component';
+
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatListModule } from '@angular/material/list'
 
 @NgModule({
   declarations: [ // aqui ficam os componentes que serão renderizados no index.html
     AppComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    NavComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MatToolbarModule // precisei importar para utilizar o material instalado
+    MatToolbarModule, // precisei importar para utilizar o material instalado
+    MatSidenavModule, // importei
+    MatListModule // importei
   ],
   providers: [],
   bootstrap: [AppComponent]
