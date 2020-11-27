eeaf151174b5b7c2d02b3474217b89399d5d534c
Rodrigo <rodrigorochasantos@outlook.com>
2020-11-27 17:08:47 -0300

    Implementando formulario de update e criacao dos metodos updateProduct e cancel no arquivo product-update.component.html

--- frontend/src/app/components/product/product-update/product-update.component.ts
+++ frontend/src/app/components/product/product-update/product-update.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Product } from '../product.model';
+import { ProductService } from '../product.service';
 
 @Component({
   selector: 'app-product-update',
@@ -7,9 +10,36 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductUpdateComponent implements OnInit {
 
-  constructor() { }
+  product!: Product;
+
+  constructor(
+    private productService: ProductService,
+    private router: Router,
+    private route: ActivatedRoute
+  ) { }
 
   ngOnInit(): void {
+    /**
+     * Esse id é passado como parâmetro, assim como foi definido
+     * no app-routing.module.ts. Tal parâmetro é passado no
+     * product-read.component.html, na parte de Actions
+     */
+    const id = this.route.snapshot.paramMap.get('id') || "0"
+    this.productService.readById(id)
+      .subscribe(product => {
+        this.product = product
+      })
   }
 
+  updateProduct(): void {
+    this.productService.update(this.product)
+      .subscribe(() => {
+        this.productService.showMessage('Produto atualizado com sucesso!')
+        this.router.navigate(['/products'])
+      })
+  }
+
+  cancel(): void {
+    this.router.navigate(['/products'])
+  }
 }
